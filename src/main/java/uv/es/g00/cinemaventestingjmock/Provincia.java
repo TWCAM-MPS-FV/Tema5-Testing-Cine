73650557ba7ec5fae166525e7db85a16f28609f0
Felipe Alejandro Valencia Bacca <fevabac@alumni.uv.es>
2022-11-20 22:11:43 +0100

    Comit inicial

---
+++ src/main/java/uv/es/g00/cinemaventestingjmock/Provincia.java
@@ -0,0 +1,19 @@
+package uv.es.g00.cinemaventestingjmock;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jopana
+ */
+public interface Provincia {
+    
+    public ArrayList<String> ListarPoblaciones();
+    public void addPoblacion (String poblacion);
+    
+}
